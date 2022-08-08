1e129a648b1c70ad308ffb50d61b083956289baa
James Ferguson <jamesferguson497@gmail.com>
2022-08-08 18:12:39 +0200

    Fix simple memory key value datastore tests

--- src/components/simple-memory-key-value-datastore/simple-memory-key-value-datastore.test.ts
+++ src/components/simple-memory-key-value-datastore/simple-memory-key-value-datastore.test.ts
@@ -3,45 +3,34 @@ import { SimpleMemoryKeyValueDatastore } from './simple-memory-key-value-datasto
 import { SimpleMemoryKeyValueEntry } from '../../types/config';
 
 describe('SimpleMemoryKeyValueDatastore', () => {
-  let configSubject$: Subject<SimpleMemoryKeyValueEntry>;
-
-  beforeEach(() => {
-    configSubject$ = new Subject();
-  });
-
   it('initialize creates a datastore', async () => {
-    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key', configSubject$);
+    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key');
     expect(datastore).toBeInstanceOf(SimpleMemoryKeyValueDatastore);
-    await datastore.cleanup();
   });
 
   it('throws an error when getting a key that does not exist', async () => {
-    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key', of())
+    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key');
     await expect(datastore.get('a')).rejects
       .toThrow(`Could not get key "a" from SimpleMemoryKeyValueDatastore because it didn't exist`);
-    await datastore.cleanup();
   });
 
   it('stores data', async () => {
-    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key', of())
+    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key')
     await datastore.put('a', Buffer.from('hello'));
     expect(await datastore.get('a'))
-    await datastore.cleanup();
   });
 
   it('deletes data', async () => {
-    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key', of())
+    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key')
     await datastore.put('a', Buffer.from('hello'));
     await datastore.drop('a');
     await expect(datastore.get('a')).rejects
       .toThrow(`Could not get key "a" from SimpleMemoryKeyValueDatastore because it didn't exist`);
-    await datastore.cleanup();
   });
 
   it('throws an error when deleting a key that does not exist', async () => {
-    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key', of())
+    const datastore = await SimpleMemoryKeyValueDatastore.initialize('key')
     await expect(datastore.drop('a')).rejects
       .toThrow(`Could not delete key "a" from SimpleMemoryKeyValueDatastore because it didn't exist`);
-    await datastore.cleanup();
   });
 });
--- src/components/simple-memory-key-value-datastore/simple-memory-key-value-datastore.ts
+++ src/components/simple-memory-key-value-datastore/simple-memory-key-value-datastore.ts
@@ -3,36 +3,22 @@ import {
   KeyValueDatastore as KeyValueDatastoreFacade
 } from '../../facades/key-value-datastore';
 import { BaseFacade, FACADE_FLAGS } from '../../facades/scaffolding/base-facade';
-import { Observable } from 'rxjs';
-import { SimpleMemoryKeyValueEntry } from '../../types/config';
-import { finalize } from 'rxjs/operators';
 
 export class SimpleMemoryKeyValueDatastore implements BaseFacade, KeyValueDatastoreFacade {
   readonly [FACADE_FLAGS]: KeyValueDatastoreFacade[typeof FACADE_FLAGS] = {
     [KEY_VALUE_DATASTORE_FLAG]: this,
   };
 
-  // static async initialize(
-  //   key: string,
-  //   config$: Observable<SimpleMemoryKeyValueEntry>,
-  // ): Promise<SimpleMemoryKeyValueDatastore> {
-  //   return new SimpleMemoryKeyValueDatastore(key, config$);
-  // }
+  static async initialize(
+    key: string,
+  ): Promise<SimpleMemoryKeyValueDatastore> {
+    return new SimpleMemoryKeyValueDatastore(key);
+  }
 
   private readonly storage: Map<string, ArrayBuffer> = new Map();
 
-  // private readonly subscription = this.config$.pipe(
-  //   finalize(() => {
-  //     // Clean up resources
-  //     // This is pretty pointless, but it represents what we would actually do
-  //     // with real resources at this point.
-  //     this.storage.clear();
-  //   }),
-  // ).subscribe();
-
-  constructor(
+  private constructor(
     private readonly key: string,
-    // private readonly config$: Observable<SimpleMemoryKeyValueEntry>,
   ) {}
 
   async put(key: string, value: ArrayBuffer): Promise<void> {
@@ -53,8 +39,4 @@ export class SimpleMemoryKeyValueDatastore implements BaseFacade, KeyValueDatast
       throw new Error(`Could not delete key "${key}" from SimpleMemoryKeyValueDatastore because it didn't exist`);
     }
   }
-
-  // async cleanup() {
-  //   this.subscription.unsubscribe();
-  // }
 }
